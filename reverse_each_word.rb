5a9e3bf6099a8d4867e7d18440e5204879d41e41
Plimon Howard V <plimon.howard@gmail.com>
2020-09-14 22:36:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(stringR)
   indx = 0
-  myArray = stringR.split
+  myArray = stringR.split(//)
   finlArry = []
   while indx < myArray.length
     finlArry << myArray[myArray.size - indx]
