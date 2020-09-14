d7e0800c11fd3e63ce33d06390487501e277e54a
Plimon Howard V <plimon.howard@gmail.com>
2020-09-14 22:44:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(stringR)
   indx = 0
   myArray = stringR.split(/ /)
   fnlArry = []
-  myArray.map do |word|
+  myArray.collect do |word|
     fnlArry << word.reverse
   end
   return fnlArry.join(" ")
